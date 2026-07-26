bbf202d5d613a488b7670973bf6f629539b8ebc0
Lemuel Louise Metante <lemuelayaso1@gmail.com>
2026-07-26 23:41:29 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,7 +108,7 @@ export default async function RootLayout({
           as="body"
           background="page"
           fillWidth
-          style={{ minHeight: "100vh", overflow: "visible" }}
+          style={{ minHeight: "100vh" }}
           margin="0"
           padding="0"
           horizontal="center"
