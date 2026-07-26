8543c20e3beaaf408cfefe7c623abadb3e649116
Lemuel Louise Metante <lemuelayaso1@gmail.com>
2026-07-26 10:41:52 +0800

    Add files via upload

---
+++ src/app/template.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { RevealFx } from "@once-ui-system/core";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <RevealFx translateY="12" fillWidth horizontal="center">
+      {children}
+    </RevealFx>
+  );
+}
